15ce1746fd359d2e4738d97eadddd50595cab582
Nuthan Prasad <nuthan1159@gmail.com>
2022-11-03 21:08:52 +0530

    test file change

--- test/SlabGrade.test.js
+++ test/SlabGrade.test.js
@@ -2,12 +2,12 @@ const { expect } = require("chai");
 const { ethers } = require('hardhat');
 
 describe('SlabGradeV1', () => {
-    let SlabGradeV1, slabGradeV1, addr1, addr2, addr3;
+    let SlabGradeV2, slabGradeV2, addr1, addr2, addr3;
      
     
         beforeEach(async () => {
-            SlabGradeV1 = await ethers.getContractFactory("SlabGradesV1");
-            slabGradeV1 = await SlabGradeV1.deploy();
+            SlabGradeV2 = await ethers.getContractFactory("SlabGradesV2");
+            slabGradeV2 = await SlabGradeV2.deploy();
             [addr1, addr2, addr3] = await ethers.getSigners();
         })
     
@@ -15,7 +15,7 @@ describe('SlabGradeV1', () => {
     describe('deployment', () => {
 
         it('deployed contract', async () => {
-            console.log(" deployment address : ", slabGradeV1.address)
+            console.log(" deployment address : ", slabGradeV2.address)
         });
         
     });
